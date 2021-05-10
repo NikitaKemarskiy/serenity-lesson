9a2759d6832e2d54e74d8edc5b993e1458fcb67a
Nikita Kemarskiy <nikitakemarskiy@gmail.com>
2021-05-10 14:25:31 +0300

    Config was updated (properties are stored as env variables)

--- src/test/java/org/nikita/config/Config.java
+++ src/test/java/org/nikita/config/Config.java
@@ -1,8 +1,6 @@
 package org.nikita.config;
 
-import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
@@ -14,6 +12,19 @@ public class Config {
         try (FileInputStream fileInputStream = new FileInputStream(configPath)) {
             properties = new Properties();
             properties.load(fileInputStream);
+
+            /**
+             * Iterate through properties to
+             * find env variable values and
+             * replace them
+             */
+            for (String key : properties.stringPropertyNames()) {
+                String value = properties.getProperty(key);
+
+                if (value.startsWith("$")) {
+                    properties.setProperty(key, System.getenv(value));
+                }
+            }
         } catch (IOException err) {
             System.err.println(err);
         }
